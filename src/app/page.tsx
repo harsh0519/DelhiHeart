9f47d312d53747208b9f013d7e4eff725685ac1b
Piyush Johnson <piyushjohnson1998@gmail.com>
2025-08-12 16:25:09 +0530

    Opt out fetch caching for sanity queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ import { menuItemsQuery } from "@/sanity/queries/menuItemsQuery"
 import { menuCategoriesQuery } from "@/sanity/queries/menuCategoriesQuery"
 
 export default async function Home() {
-  const bannerImages = await client.fetch(bannerquery)
-  const offers = await client.fetch(getOffersQuery)
-  const menuItems = await client.fetch(menuItemsQuery)
-  const menuCategories = await client.fetch(menuCategoriesQuery)
+  const bannerImages = await client.fetch(bannerquery,{},{cache: 'no-store'})
+  const offers = await client.fetch(getOffersQuery,{},{cache: 'no-store'})
+  const menuItems = await client.fetch(menuItemsQuery,{},{cache: 'no-store'})
+  const menuCategories = await client.fetch(menuCategoriesQuery,{},{cache: 'no-store'})
 
   return (
     <div>
